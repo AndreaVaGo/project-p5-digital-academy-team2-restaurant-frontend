642e092f5851a001f4fdfad58e3971c5ce81b4b9
Ruddy <ruddy391@gmail.com>
2026-09-10 14:31:09 +0200

    feat: create reservation view

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../Views/LoginView.vue'
-import RegisterView from '../Views/RegisterView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../Views/LoginView.vue";
+import RegisterView from "../Views/RegisterView.vue";
+import ReservationView from "../Views/ReservationView.vue";
 
 const routes = [
-  { path: '/', name: 'login', component: LoginView, alias: '/login' },
-  { path: '/register', name: 'register', component: RegisterView },
-]
+  { path: "/", name: "login", component: LoginView, alias: "/login" },
+  { path: "/register", name: "register", component: RegisterView },
+  {
+    path: "/reservation",
+    name: "reservation",
+    component: ReservationView,
+  },
+];
 
 export default createRouter({
   history: createWebHistory(),
   routes,
-})
+});
