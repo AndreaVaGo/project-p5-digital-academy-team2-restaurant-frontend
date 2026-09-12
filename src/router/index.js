b36c0d12f892539de273938352b2814dcb8e3b07
Ruddy <ruddy391@gmail.com>
2026-09-13 00:36:20 +0200

    feat: add reservation route to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/Home.vue'
-import LoginView from '../Views/LoginView.vue'
-import RegisterView from '../Views/RegisterView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import LoginView from "../Views/LoginView.vue";
+import RegisterView from "../Views/RegisterView.vue";
 
 const routes = [
-  { path: '/', name: 'home', component: Home, alias: '/home' },
-  { path: '/login', name: 'login', component: LoginView, alias: '/login' },
-  { path: '/register', name: 'register', component: RegisterView },
-]
+  { path: "/", name: "home", component: Home, alias: "/home" },
+  { path: "/login", name: "login", component: LoginView, alias: "/login" },
+  { path: "/register", name: "register", component: RegisterView },
+  { path: "/reservation", name: "reservation", component: ReservationView, alias: "/reservation"},
+];
 
 export default createRouter({
   history: createWebHistory(),
