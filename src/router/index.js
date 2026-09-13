dfd2d6aa001ce071eec849ad306cfb04636d6830
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-09-13 15:11:37 +0200

    feat: add nested admin routes for dashboard, products, orders and billing

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import LoginView from '../Views/LoginView.vue'
 import RegisterView from '../Views/RegisterView.vue'
-import AdminWelcomeView from '../Views/AdminWelcomeView.vue'
 import AdminLayout from '../Views/AdminLayout.vue'
 import AdminDashboardView from '../Views/AdminDashboardView.vue'
 import AdminProductsView from '../Views/AdminProductsView.vue'
@@ -13,7 +12,6 @@ const routes = [
   { path: '/', name: 'home', component: Home, alias: '/home' },
   { path: '/login', name: 'login', component: LoginView, alias: '/login' },
   { path: '/register', name: 'register', component: RegisterView },
-  { path: '/admin/welcome', name: 'admin-welcome', component: AdminWelcomeView },
   {
     path: '/admin',
     component: AdminLayout,
