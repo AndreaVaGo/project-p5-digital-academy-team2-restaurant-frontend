f14b78f1baf1a1938c6bce3431e926ded0ff4acb
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-09-13 19:23:13 +0200

    Add routes for customer profile and account views

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/Home.vue'
-import LoginView from '../Views/LoginView.vue'
-import RegisterView from '../Views/RegisterView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import LoginView from "../Views/LoginView.vue";
+import RegisterView from "../Views/RegisterView.vue";
+import MyAccountView from "../Views/MyAccountView.vue";
+import CustomerProfileView from "../Views/CustomerProfileView.vue";
 
 const routes = [
-  { path: '/', name: 'home', component: Home, alias: '/home' },
-  { path: '/login', name: 'login', component: LoginView, alias: '/login' },
-  { path: '/register', name: 'register', component: RegisterView },
-]
+  { path: "/", name: "home", component: Home, alias: "/home" },
+  { path: "/login", name: "login", component: LoginView, alias: "/login" },
+  { path: "/register", name: "register", component: RegisterView },
+  { path: "/account", name: "account", component: MyAccountView },
+  {
+    path: "/account/profile",
+    name: "customer-profile",
+    component: CustomerProfileView,
+  },
+];
 
 export default createRouter({
   history: createWebHistory(),
   routes,
-})
+});
