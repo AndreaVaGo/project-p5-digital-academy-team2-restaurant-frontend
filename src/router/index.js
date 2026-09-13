7f2ce63b3cd8d16791b2eb57f45006909de62a60
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-09-13 13:40:31 +0200

    feat: add admin welcome view with route and lucide icons

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
 import LoginView from '../Views/LoginView.vue'
 import RegisterView from '../Views/RegisterView.vue'
+import AdminWelcomeView from '../Views/AdminWelcomeView.vue'
 
 const routes = [
   { path: '/', name: 'home', component: Home, alias: '/home' },
   { path: '/login', name: 'login', component: LoginView, alias: '/login' },
   { path: '/register', name: 'register', component: RegisterView },
+  { path: '/admin/welcome', name: 'admin-welcome', component: AdminWelcomeView },
 ]
 
 export default createRouter({
