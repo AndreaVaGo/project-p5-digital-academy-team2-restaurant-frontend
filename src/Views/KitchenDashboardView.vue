6e9dcd07b66920394db8485485f0c39b0082386e
Jenny Sánchez Requejo <jenny25sr@gmail.com>
2026-09-14 12:12:57 +0200

    feat: add kitchen dashboard route and empty view

---
+++ src/Views/KitchenDashboardView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <div class="p-6">
+        <h1 class="font-headline text-3xl font-semibold text-primary">
+            Dashboard Cocina
+        </h1>
+    </div>
+</template>
